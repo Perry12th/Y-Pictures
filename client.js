c96350998c83f6a93a41c9a66031840501344e05
sharpjd <sharpieinthebox@gmail.com>
2022-11-27 10:50:25 -0500

    (debug) client.js freezes to wait for request - removed an alert after clicking button

--- client.js
+++ client.js
@@ -20,18 +20,17 @@ window.onload = function(){
 }
 
 function debug_request(){
-    alert("I have been clicked");
-
     //get the birb video
     var request = JSON.stringify({
         'name':identity,
         'action':'getBirb'})
-    xhttp.open("POST", serverLocation+"?data="+request, true);
+    xhttp.open("POST", serverLocation+"?data="+request, false); //stop the thread just for debugging purposes
     xhttp.send();
 
     //add a video element if you get the file
-    if(true /*xhttp.status == 200*/){ 
+    if(xhttp.status == 200){ 
         //TODO the page *is* receiving data (5mb video according to inspect element) but how do you parse it into something you can view???
+        alert("successfully received data");
         var video = document.createElement("video");
         $(video).attr("src", xhttp.response);
         $("#content").append(video);
